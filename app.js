24f2039fae718245659684e5170a4ebf28a620fe
kartiktya <kartiktyagi953@gmail.com>
2024-02-13 02:51:53 +0530

    21_cartWithManyToManyRelationship

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const sequelize = require('./util/database.js');    //sequlize is an object impo
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cartItem')
 
 const app = express();
 
@@ -39,7 +41,14 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
-sequelize.sync()
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+sequelize
+.sync()
 //.sync({ force: true })
 .then( result => {
     return User.findByPk(1);
@@ -48,11 +57,15 @@ sequelize.sync()
 .then(user => {
     if(!user) {
         return User.create({ name:'Amit', email: 'amit@gmail.com'});
-    }
+    }   
     return user;
 })
 .then(user => {
-    console.log(user);
+    //console.log(user);
+    return user.createCart()
+    
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => console.log(err));
